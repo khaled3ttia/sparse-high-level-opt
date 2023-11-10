bc407954001d75f6a5a467968ec49cd70d3334ea
Khaled Abdelaal <khaled.abdelaal@ou.edu>
2023-11-10 16:17:24 -0600

    scripts to find which Kronecker graphs to generate (in terms of parameters) based on storage requirements. Then, use krongen to generate the graphs

---
+++ find_valid_sizes/generate.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+import glob
+import argparse
+from pathlib import Path
+import subprocess 
+import swifter 
+import os
+
+def run_cmd(row, krongen,k_val,outdir):
+    outfile = f"k{k_val}_{row['x0']:.3f}_{row['x1']:.3f}_{row['x2']:.3f}_{row['x3']:.3f}"
+    cmd = f"""{krongen} -o:{outdir}/{outfile}.txt -m:"{row['x0']:.3f} {row['x1']:.3f}; {row['x2']:.3f} {row['x3']:.3f}" -i:{k_val} """
+    print(cmd)
+    result_gen = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    return result_gen.returncode
+
+def main(args):
+    
+
+    indir = args.indir
+    krongen = args.krongen
+    outdir = args.outdir 
+    os.makedirs(outdir, exist_ok=True)
+    files_to_process = glob.glob(f'{indir}/*.stats')
+
+    for file in files_to_process:
+        df = pd.read_csv(file)
+        k_val = int(f"{Path(file).stem.split('_')[1]}")
+        to_generate = df[df['generate']].copy()
+
+        if not to_generate.empty:
+            print(f'to_generate : {to_generate}')
+            print(len(to_generate))
+            to_generate['exit_code'] = to_generate.swifter.set_dask_scheduler('processes').set_npartitions(16).allow_dask_on_strings(enable=True).apply(run_cmd, args=(krongen,k_val,outdir,), axis=1)
+            print(to_generate['exit_code']) 
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--indir', type=str, default='.', help="Input directory for stats files")
+    parser.add_argument('--krongen', type=str, default='/home/khaled/Documents/snap/examples/krongen/krongen', help="Full path for krongen binary (including the binary)")
+    parser.add_argument('--outdir', type=str, default='./mtx_files', help="Directory to store output mtx files")
+
+    args = parser.parse_args()
+    main(args)
