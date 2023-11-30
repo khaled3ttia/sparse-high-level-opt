20195f4aa2a4dca95504520709d1a3f2c576c736
Khaled Abdelaal <khaled.abdelaal@ou.edu>
2023-11-30 15:24:29 -0600

    merge all filtered entries (parameters to be generated) in one dataframe, and then use dask on it

--- find_valid_sizes/generate.py
+++ find_valid_sizes/generate.py
@@ -8,9 +8,9 @@ import dask.dataframe as dd
 import os
 from dask.distributed import Client, LocalCluster
 
-def run_cmd(row, krongen,k_val,outdir):
-    outfile = f"k{k_val}_{row['x0']:.3f}_{row['x1']:.3f}_{row['x2']:.3f}_{row['x3']:.3f}"
-    cmd = f"""{krongen} -o:{outdir}/{outfile}.txt -m:"{row['x0']:.3f} {row['x1']:.3f}; {row['x2']:.3f} {row['x3']:.3f}" -i:{k_val} """
+def run_cmd(row, krongen,outdir):
+    outfile = f"k{row['k']}_{row['x0']:.3f}_{row['x1']:.3f}_{row['x2']:.3f}_{row['x3']:.3f}"
+    cmd = f"""{krongen} -o:{outdir}/{outfile}.txt -m:"{row['x0']:.3f} {row['x1']:.3f}; {row['x2']:.3f} {row['x3']:.3f}" -i:{row['k']} """
     print(cmd)
     result_gen = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
@@ -34,22 +34,22 @@ def main(args):
     cluster = LocalCluster()
     client = Client(cluster)
 
-    def run_cmd_dask(df): return df.apply(run_cmd, args=(krongen,k_val,outdir,), axis=1)
+    def run_cmd_dask(df): return df.apply(run_cmd, args=(krongen,outdir,), axis=1)
 
+    full_to_generate = pd.DataFrame()
     for file in files_to_process:
         df = pd.read_csv(file)
         k_val = int(f"{Path(file).stem.split('_')[1]}")
         to_generate = df[df['generate']].copy()
-        count_generate = len(to_generate.index)
-        
-        to_generate_d = dd.from_pandas(to_generate, npartitions=int(count_generate/nthreads))
-        if not to_generate.empty:
-            print(f'to_generate : {to_generate}')
-            print(len(to_generate))
-            #to_generate['exit_code'] = to_generate.swifter.set_dask_scheduler('processes').set_npartitions(16).allow_dask_on_strings(enable=True).apply(run_cmd, args=(krongen,k_val,outdir,), axis=1)
-            to_generate_d.map_partitions(run_cmd_dask).compute(scheduler='threads')  
-            #print(to_generate['exit_code']) 
+        full_to_generate = pd.concat([full_to_generate,to_generate],ignore_index=True)
+    count_generate = len(full_to_generate.index)
+    print(f'generating {count_generate} graphs using {nthreads}...')
 
+    to_generate_d = dd.from_pandas(full_to_generate, npartitions=int(count_generate/nthreads))
+    if not full_to_generate.empty:
+        #to_generate['exit_code'] = to_generate.swifter.set_dask_scheduler('processes').set_npartitions(16).allow_dask_on_strings(enable=True).apply(run_cmd, args=(krongen,k_val,outdir,), axis=1)
+        to_generate_d.map_partitions(run_cmd_dask).compute(scheduler='threads')  
+        #print(to_generate['exit_code']) 
 
 if __name__ == '__main__':
 
