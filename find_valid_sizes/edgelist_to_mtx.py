750b0d3134f9d0f9241e73270f9c15f4e60f78b9
Khaled Abdelaal <khaled.abdelaal@ou.edu>
2024-01-03 11:33:02 -0600

    copied the utility script to convert edgelist to mtx files

---
+++ find_valid_sizes/edgelist_to_mtx.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+import scipy.io as spio
+import math
+import os
+import argparse
+
+def main(edgelist):
+    data = pd.read_csv(edgelist, sep='\t',comment='#', names=['from','to'])
+
+    basename = os.path.basename(edgelist)
+    dirname = os.path.dirname(edgelist)
+
+    basename_no_ext = os.path.splitext(basename)[0]
+    k_val = int(basename_no_ext.split('_')[0].replace('k',''))
+    nodes = int(math.pow(2, k_val))
+
+    rows = data['from']
+    cols = data['to']
+
+    ones = np.ones(len(rows), np.float32)
+    matrix = sp.coo_matrix((ones, (rows,cols)), (nodes,nodes))
+
+
+    spio.mmwrite(f'{dirname}/{basename_no_ext}.mtx',matrix)
+
+
+if __name__ == '__main__':
+    parser =  argparse.ArgumentParser()
+
+    parser.add_argument('--i', type=str, help='Input Edge List File Path', required=True)
+    args = parser.parse_args()
+
+    main(args.i)
