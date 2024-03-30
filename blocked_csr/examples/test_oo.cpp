6c68f10fe32548aabbaaf72de3b1a6d1f825779a
Khaled Abdelaal <khaled.abdelaal@ou.edu>
2024-03-29 20:43:30 -0500

    add verification using the MKL SpMV routine

--- blocked_csr/examples/test_oo.cpp
+++ blocked_csr/examples/test_oo.cpp
@@ -5,10 +5,12 @@ void verify(std::vector<float> gold, std::vector<float> actual, float max_err) {
     std::cout << "Sizes do not match. Verification failed!" << std::endl;
     return;
   }
+  std::cout << "Output size: " << gold.size() << std::endl;
 
   int count_errors = 0;
   for (int i = 0; i < actual.size(); ++i) {
     if (abs(actual[i] - gold[i]) > max_err) {
+      std::cout << "Error: at row (" << i << "): " << actual[i] << " " << gold[i] << std::endl;
       count_errors++;
     }
   }
@@ -42,6 +44,30 @@ void evaluateGold(const SpMat<T> &mat, const std::vector<T> &denseVec,
                  1.0f, 0.0f, result);
 }
 
+void evaluateGoldMKL(SpMat<float> &mat, const std::vector<float>& denseVec, std::vector<float>& result){
+    std::vector<float> values = mat.values();
+    std::vector<int> rows = mat.rows();
+    std::vector<int> cols = mat.cols();
+
+    std::vector<int> rowPtrs(mat.nrows() + 1, 0);
+    std::vector<int> colIndices(mat.nnz());
+    std::vector<float> vals(mat.nnz());
+
+    coo_to_csr_scipy_v(mat.nrows(), mat.ncols(), mat.nnz(), rows, cols, values, rowPtrs, colIndices, vals);
+
+    sparse_matrix_t csrA;
+
+    mkl_sparse_s_create_csr(&csrA, SPARSE_INDEX_BASE_ZERO, mat.nrows(), mat.ncols(), rowPtrs.data(), rowPtrs.data()+1, colIndices.data(), values.data());
+
+    matrix_descr descrA; 
+    descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
+    mkl_sparse_optimize(csrA);
+
+    mkl_sparse_s_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f, csrA, descrA, denseVec.data(), 0.0f, result.data());
+
+    mkl_sparse_destroy(csrA);
+}
+
 int main() {
 
   SpMat<float> myMat =
@@ -53,10 +79,25 @@ int main() {
 
   myMat.printTiles();
   fillRandom(denseVec, 0.0f, 1.0f);
+  std::cout << "Dense Vector: " << std::endl;
+  for (auto &elem: denseVec){
+      std::cout << elem << " ";
+  }
+  std::cout << std::endl;
   myMat.multiply(denseVec, result);
 
   std::vector<float> ref_result(myMat.nrows(), 0);
+  std::vector<float> mkl_result(myMat.nrows(), 0);
+
+  // Comparing tiled SpMV with ref SpMV
   evaluateGold(myMat, denseVec, ref_result);
   verify(ref_result, result, 0.001f);
+
+  // Comparing tiled SpMV with ref MKL
+  evaluateGoldMKL(myMat, denseVec, mkl_result);
+  verify(mkl_result, result, 0.001f);
+
+  // Comparing ref SpMV with ref MKL
+  verify(mkl_result, ref_result, 0.001f);
 
 }
