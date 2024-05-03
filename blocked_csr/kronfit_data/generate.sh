c81b47a954fe9dd634f9fbaf09bfe61f476911f2
Khaled Abdelaal <khaled.abdelaal@ou.edu>
2024-05-02 20:51:52 -0500

    fix paths in generate.sh script to generate kronecker graphs for SNAP

--- blocked_csr/kronfit_data/generate.sh
+++ blocked_csr/kronfit_data/generate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-CONDA_ENV_NAME=sparse-high
-conda activate $CONDA_ENV_NAME
+#CONDA_ENV_NAME=sparse-high
+#conda activate $CONDA_ENV_NAME
 
 ROOT_DIR="/home/khaled/Documents/sparse-high-level-opt"
 
-GENERATE_SCRIPT_DIR="${ROOT_DIR}/blocked_csr/kronfit_data/mtx_files"
+GENERATE_SCRIPT_DIR="${ROOT_DIR}/blocked_csr/kronfit_data"
 CONVERT_SCRIPT_DIR="${ROOT_DIR}/find_valid_sizes"
+GENERATE_SCRIPT_OUT_DIR="${GENERATE_SCRIPT_DIR}/mtx_files"
 
 # first generate graphs
 cd $GENERATE_SCRIPT_DIR
